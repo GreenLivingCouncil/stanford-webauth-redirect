58dc9c641b3604ab99f6610b91fb90b2a6e1eda1
sckoo <stephen.theking@gmail.com>
2014-12-14 21:57:56 -0800

    Init repo

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>redirecting...</title>
+    </head>
+    <body>
+        <script type="text/javascript">
+            window.location.assign("<?php echo $_GET["t"]; ?>");
+        </script>
+    </body>
+</html> 
